74cc09a337fff34e1fd525b5548365ecb4ea0345
s179133 <s179133@SEA-8E3F0F>
2017-08-23 17:44:15 -0700

    aded rotation,translation and scale to render form

--- include/graphics/DrawFunction.h
+++ include/graphics/DrawFunction.h
@@ -1,5 +1,6 @@
 #pragma once
-
+#include "glm\glm.hpp"
+#include "glm\gtc\type_ptr.hpp"
 struct Framebuffer;
 struct shader;
 struct Geometry;
@@ -13,3 +14,42 @@ void setUniform(const shader &s, int location, float value);
 void setUniform(const shader &s, int location, int value);
 
 void setUniform(const shader &s, int location, const Texture &value, unsigned slot);
+
+
+// these will be called  everytime the variadic unpackign recursion takes place
+// based upon what unifroms are passed in, the correct funcation will autopmatically
+//be calledx in so doing, the appropriate glprogramuniform funtion will also be called
+namespace __internal
+{
+	void t_setUniform(const shader &s, int &loc_io, int &tex_io, float val);
+	void t_setUniform(const shader &s, int &loc_io, int &tex_io, int val);
+	void t_setUniform(const shader &s, int &loc_io, int &tex_io, const Texture val);
+
+	void t_setUniform(const shader &s, int &loc_io, int &tex_io, const glm::vec3 &val);
+	void t_setUniform(const shader &s, int &loc_io, int &tex_io, const glm::vec4 &val);
+	void t_setUniform(const shader &s, int &loc_io, int &tex_io, const glm::mat4 &val);
+};
+
+//the rec ursive template funtion
+// u is the variadic template parameter. eachtime the fuction gets called
+// the T parameter (val) will comsume  1 element of the variadic
+// so if there is recursion(fuction calling itself), the unioform
+// will slowly be eaten by the T parameter.
+
+template<typename T, typename ...U>
+void setUniforms(const shader &s, int &loc_io, int &tex_io,
+	const T &val, U &&... uniforms)
+{
+	__internal::t_setUniform(s, loc_io, tex_io, val);
+
+	setUniforms(s, loc_io, tex_io, uniforms...);
+}
+// this base case, is the last function that gets called. since T is alwasy eating
+// valuse  from U, eventually there will be no more U. to insure that we have a valid function,
+// we need to have a  base case like so...
+template<typename T>
+void setUniforms(const shader &s, int &loc_io, int &tex_io,
+	const T &val)
+{
+	__internal::t_setUniform(s, loc_io, tex_io, val);
+}
--- source/DrawFunction.cpp
+++ source/DrawFunction.cpp
@@ -40,4 +40,43 @@ void setUniform(const shader &s, int location, const Texture &value, unsigned sl
 	glProgramUniform1i(s.handle, location, slot);
 }
 
+namespace __internal
+{
+	void t_setUniform(const shader &s, int &loc_io, int &tex_io, float val)
+	{
+		glProgramUniform1i(s.handle, loc_io++, val);
 
+	}
+	void t_setUniform(const shader &s, int &loc_io, int &tex_io, int val)
+	{
+		glProgramUniform1i(s.handle, loc_io++, val);
+
+	}
+	void t_setUniform(const shader &s, int &loc_io, int &tex_io, const Texture val)
+	{
+		glActiveTexture(GL_TEXTURE0 + tex_io);
+		glBindTexture(GL_TEXTURE_2D, val.handle);
+		glProgramUniform1i(s.handle, loc_io++, tex_io++);
+	}
+
+	void t_setUniform(const shader &s, int &loc_io, int &tex_io, const glm::vec3 &val)
+	{
+		glProgramUniform3fv(s.handle, loc_io++, 1, glm::value_ptr(val));
+
+	}
+
+
+	void t_setUniform(const shader &s, int &loc_io, int &tex_io, const glm::vec4 &val)
+	{
+		glProgramUniform4fv(s.handle, loc_io++, 1, glm::value_ptr(val));
+
+	}
+
+
+
+	void t_setUniform(const shader &s, int &loc_io, int &tex_io, const glm::mat4 &val)
+	{
+		glProgramUniformMatrix4fv(s.handle, loc_io++, 1, 0, glm::value_ptr(val));
+
+	}
+};
